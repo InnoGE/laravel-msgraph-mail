c879bccfc8650372d3c9a93265caa38c40095dd0
Tim Geisendoerfer <geisi@users.noreply.github.com>
2023-11-27 13:41:01 +0100

    Added test to verify overwriting of mail sender

--- tests/MicrosoftGraphTransportTest.php
+++ tests/MicrosoftGraphTransportTest.php
@@ -318,7 +318,7 @@ it('sends html mails with inline images with microsoft graph', function () {
                     'subject' => 'Dev Test',
                     'body' => [
                         'contentType' => 'HTML',
-                        'content' => '<b>Test</b><img src="cid:' . $inlineImageContentId . '">'.PHP_EOL,
+                        'content' => '<b>Test</b><img src="cid:'.$inlineImageContentId.'">'.PHP_EOL,
                     ],
                     'toRecipients' => [
                         [
@@ -363,6 +363,95 @@ it('sends html mails with inline images with microsoft graph', function () {
                             'contentBytes' => '/9j/4AAQSkZJRgABAQEASABIAAD//gATQ3JlYXRlZCB3aXRoIEdJTVD/2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFRT/2wBDAQMEBAUEBQkFBQkUDQsNFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBT/wgARCABLAGQDAREAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAj/xAAWAQEBAQAAAAAAAAAAAAAAAAAABQj/2gAMAwEAAhADEAAAAZ71TDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH/xAAUEAEAAAAAAAAAAAAAAAAAAABw/9oACAEBAAEFAgL/xAAUEQEAAAAAAAAAAAAAAAAAAABw/9oACAEDAQE/AQL/xAAUEQEAAAAAAAAAAAAAAAAAAABw/9oACAECAQE/AQL/xAAUEAEAAAAAAAAAAAAAAAAAAABw/9oACAEBAAY/AgL/xAAUEAEAAAAAAAAAAAAAAAAAAABw/9oACAEBAAE/IQL/2gAMAwEAAgADAAAAEEkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkv/xAAUEQEAAAAAAAAAAAAAAAAAAABw/9oACAEDAQE/EAL/xAAUEQEAAAAAAAAAAAAAAAAAAABw/9oACAECAQE/EAL/xAAUEAEAAAAAAAAAAAAAAAAAAABw/9oACAEBAAE/EAL/2Q==',
                             'contentId' => $inlineImageContentId,
                             'isInline' => true,
+                        ],
+                    ],
+                ],
+                'saveToSentItems' => false,
+            ]);
+
+        return true;
+    });
+});
+
+test('the configured mail sender can be overwritten', function () {
+    Config::set('mail.mailers.microsoft-graph', [
+        'transport' => 'microsoft-graph',
+        'client_id' => 'foo_client_id',
+        'client_secret' => 'foo_client_secret',
+        'tenant_id' => 'foo_tenant_id',
+        'from' => [
+            'address' => 'taylor@laravel.com',
+            'name' => 'Taylor Otwell',
+        ],
+    ]);
+    Config::set('mail.default', 'microsoft-graph');
+
+    Cache::set('microsoft-graph-api-access-token', 'foo_access_token', 3600);
+
+    Http::fake();
+
+    $mailable = new TestMail(false);
+    $mailable->from('other-mail@laravel.com', 'Other Mail');
+
+    Mail::to('caleb@livewire.com')
+        ->bcc('tim@innoge.de')
+        ->cc('nuno@laravel.com')
+        ->send($mailable);
+
+    Http::assertSent(function (Request $value) {
+        expect($value)
+            ->url()->toBe('https://graph.microsoft.com/v1.0/users/taylor@laravel.com/sendMail')
+            ->hasHeader('Authorization', 'Bearer foo_access_token')->toBeTrue()
+            ->body()->json()->toBe([
+                'message' => [
+                    'subject' => 'Dev Test',
+                    'body' => [
+                        'contentType' => 'Text',
+                        'content' => 'Test'.PHP_EOL,
+                    ],
+                    'toRecipients' => [
+                        [
+                            'emailAddress' => [
+                                'address' => 'caleb@livewire.com',
+                            ],
+                        ],
+                    ],
+                    'ccRecipients' => [
+                        [
+                            'emailAddress' => [
+                                'address' => 'nuno@laravel.com',
+                            ],
+                        ],
+                    ],
+                    'bccRecipients' => [
+                        [
+                            'emailAddress' => [
+                                'address' => 'tim@innoge.de',
+                            ],
+                        ],
+                    ],
+                    'replyTo' => [],
+                    'sender' => [
+                        'emailAddress' => [
+                            'address' => 'other-mail@laravel.com',
+                        ],
+                    ],
+                    'attachments' => [
+                        [
+                            '@odata.type' => '#microsoft.graph.fileAttachment',
+                            'name' => 'test-file-1.txt',
+                            'contentType' => 'text',
+                            'contentBytes' => 'Zm9vCg==',
+                            'contentId' => 'test-file-1.txt',
+                            'isInline' => false,
+                        ],
+                        [
+                            '@odata.type' => '#microsoft.graph.fileAttachment',
+                            'name' => 'test-file-2.txt',
+                            'contentType' => 'text',
+                            'contentBytes' => 'Zm9vCg==',
+                            'contentId' => 'test-file-2.txt',
+                            'isInline' => false,
                         ],
                     ],
                 ],
